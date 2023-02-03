349b79a3a04e469f77a645fcaf410f8666551472
mingg123 <spgb123spgb@gmail.co.kr>
2023-02-03 17:51:10 +0900

    init commit

---
+++ apple-login/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>hhihi</div>
+      <div
+        id="appleid-signin"
+        data-mode="center-align"
+        data-color="black"
+        data-border="false"
+        data-border-radius="50"
+        data-size="42"
+      ></div>
+    </>
+  );
+}
+
+export default App;

87dfa834548547b8d2b020b316f2724750a57e62
mingg123 <spgb123spgb@gmail.co.kr>
2023-04-20 15:59:06 +0900

    feat: add

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ Sentry.init({
     new Sentry.BrowserTracing(),
     new Sentry.Integrations.Breadcrumbs({ console: true }),
   ],
-  release: "apple6.0",
+  release: "apple7.0",
   tracesSampleRate: 1.0,
 });
 
